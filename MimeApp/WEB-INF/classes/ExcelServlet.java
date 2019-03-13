d648916c880efc5a28e03098b93f292451acc4b2
nemadroshan <nemadroshan@gmail.com>
2019-03-13 17:49:42 +0530

    commit1(mime tags)

---
+++ MimeApp/WEB-INF/classes/ExcelServlet.java
@@ -0,0 +1,30 @@
+package com.nt.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+
+public class ExcelServlet extends HttpServlet{
+	
+	public void service(HttpServletRequest req, HttpServletResponse res)throws ServletException,IOException{
+
+		//get PrintWriter
+		PrintWriter pw =res.getWriter();
+		//set Response Content tYpe
+
+		res.setContentType("application/vnd.ms-excel");
+		//write logic to generate to output(webpage)
+		pw.println("<table border ='0'>");
+		pw.println("<tr><th>Leader</th><th>plcaces</th><th>Winning Prediction</th></tr>");
+		pw.println("<tr><td>Modi</td><td>Gujarat</td><td>Super win</td></tr>");
+		pw.println("<tr><td>Rahul</td><td>Delhi</td><td>May be  win</td></tr>");
+		pw.println("<tr><td>Pawan kalyan</td><td>Telngana</td><td>Super win</td></tr>");
+		pw.println("<tr><td>Mamata Banerji</td><td>West Bengal</td><td> so so win</td></tr>");
+		pw.println("<tr><td>Raj Thakrey</td><td>Maharashtra</td><td> ONly one sit will win</td></tr>");
+		pw.println("</table>");
+
+		//close Streeam object
+		pw.close();
+	}//service();
+}//class
